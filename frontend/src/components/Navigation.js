4405766c86ec3e9fbac65dbb42fdef981b7108bc
ValeriiaRudenko <99447413+ValeriaRudenko@users.noreply.github.com>
2023-12-01 23:54:19 +0200

    added profile page

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import SignUp from './SignUp';
 import SignIn from './SignIn';
+import Profile from './Profile';
 
 const Navigation = () => {
     const [currentPage, setCurrentPage] = useState('signUp');
@@ -9,9 +10,11 @@ const Navigation = () => {
         <div>
             <button onClick={() => setCurrentPage('signUp')}>Sign Up</button>
             <button onClick={() => setCurrentPage('signIn')}>Sign In</button>
+            <button onClick={() => setCurrentPage('profile')}>Profile</button>
 
             {currentPage === 'signUp' && <SignUp />}
             {currentPage === 'signIn' && <SignIn />}
+            {currentPage === 'profile' && <Profile />}
         </div>
     );
 };
