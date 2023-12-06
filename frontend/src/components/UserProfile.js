d15886c2ecfeaf07df47bf9710c344a489ae8a2a
ValeriiaRudenko <99447413+ValeriaRudenko@users.noreply.github.com>
2023-12-06 23:14:27 +0200

    Create user profile page basic

---
+++ frontend/src/components/UserProfile.js
@@ -0,0 +1,131 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import './Profile.css';
+import './Sign.css';
+// ... (import statements)
+import {usePageContext} from './PageContext';
+
+const UserProfile = () => {
+    const [avatar, setAvatar] = useState(null);
+    const [breed, setBreed] = useState('');
+    const [age, setAge] = useState('');
+    const [pettype, setPetType] = useState('');
+    const [nickname, setNickname] = useState('User123');
+    const [posts, setPosts] = useState('');
+    const [isFollowed, setIsFollowed] = useState(false); // Assume initial state, you can set it based on your logic
+
+    const handleFollowButtonClick = () => {
+        // Toggle follow status
+        setIsFollowed(!isFollowed);
+    };
+
+    useEffect(() => {
+        // Fetch user profile data when the component mounts
+        fetchProfileData();
+    }, []); // Empty dependency array ensures the effect runs only once
+    const {handlePageChange} = usePageContext();
+
+    const fetchProfileData = async () => {
+        try {
+            // Get the token from wherever you stored it (e.g., session storage)
+            const token = sessionStorage.getItem('token');
+            // Make a GET request to the server endpoint with the token in the headers
+            const response = await axios.get('http://localhost:5000/api/profile', {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            // Extract data from the response
+            const userData = response.data;
+            // Update state with the fetched data
+            setAge(userData.age);
+            setBreed(userData.breed);
+            setPetType(userData.pettype);
+
+            // Fetch the avatar image
+            // const avatarResponse = await axios.get('http://localhost:5000/api/avatar', {
+            //     headers: {
+            //         Authorization: `Bearer ${token}`,
+            //     }});
+            fetch('http://localhost:5000/api/avatar', {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.blob();
+                })
+                .then(blob => {
+                    if (blob.type.startsWith('image/jpeg') || blob.type.startsWith('image/png')) {
+                        // Check if the blob is of type JPEG or PNG
+                        const objectURL = URL.createObjectURL(blob);
+                        setAvatar(objectURL);
+                    } else {
+                        throw new Error('The response is not a JPEG or PNG image.');
+                    }
+                })
+                .catch(error => {
+                    console.error('Error fetching image:', error);
+                });
+
+        } catch (error) {
+            console.error('Error fetching profile data:', error.message || error);
+        }
+    };
+
+    return (
+        <div className="row">
+            <div className="col"></div>
+            <div className="col d-flex justify-content-center">
+                <div id="profile-container" className="container">
+                    <form>
+                        {/* Display the person's nickname at the top of the container */}
+                        <div className="nickname-container">
+                            <h3>{nickname}</h3>
+                        </div>
+
+                        <img
+                            id="avatar-image"
+                            className={avatar ? 'avatar-with-image' : ''}
+                            src={avatar}
+                            alt="Avatar"
+                        />
+                        <div>
+                            <div>
+                                {/* Conditionally render "Follow" or "Unfollow" button */}
+                                {isFollowed ? (
+                                    <label className="upload-label" id="upload-label" htmlFor="unfollow-label"
+                                           onClick={handleFollowButtonClick}>
+                                        Unfollow
+                                    </label>
+                                ) : (
+                                    <label className="upload-label" id="upload-label" htmlFor="follow-label"
+                                           onClick={handleFollowButtonClick}>
+                                        Follow
+                                    </label>
+                                )}
+                            </div>
+                            <div>
+                                <p>Pet type: {pettype}</p>
+                                <p>Breed: {breed}</p>
+                                <p>Age: {age}</p>
+                            </div>
+
+                            {/* Additional profile details can be added here */}
+
+                        </div>
+                    </form>
+                    <div className="posts-container">
+                        <h4>Posts:</h4>
+                    </div>
+                </div>
+            </div>
+            <div className="col"></div>
+        </div>
+    );
+};
+
+export default UserProfile;
