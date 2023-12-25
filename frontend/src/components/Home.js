73e869318d81f7923151ed91ee465693981cf709
ValeriiaRudenko <99447413+ValeriaRudenko@users.noreply.github.com>
2023-12-25 20:49:41 +0000

    Update Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -107,33 +107,29 @@ const Home = () => {
     }, [popularPosts]);
 
     return (
-        <div className="container">
-            <h2 className="text-center mb-4">Popular Posts</h2>
+        <div className="container text-center mt-5">
+            <h2 className="mb-4">Popular Posts</h2>
             <div className="row">
                 {popularPosts.map((post) => (
-                    <div key={post._id} className="col-lg-4 mb-4">
-                        <div className="card">
-                            <img className="card-img-top" src={`http://localhost:5000/api/posts/${post._id}/image`} alt="Post" />
+                    <div key={post._id} className="col-lg-4 col-md-6 mb-4">
+                        <div className="card p-3" style={{ boxShadow: '0 4px 8px rgba(0,0,0,0.1)' }}>
+                            <img className="card-img-top" src={`http://localhost:5000/api/posts/${post._id}/image`} alt="Post" style={{ maxHeight: '200px', objectFit: 'cover' }} />
                             <div className="card-body">
-
-                                <h5 className="card-title">{post.author.username}</h5><h5 className="card-title">{post.label}</h5>
-                                <p className="card-text">{post.text}</p>
+                                <h5 className="card-title mb-3">{post.label}</h5>
+                                <p className="card-text mb-2">{post.text}</p>
                                 <p className="card-text">
-                                    <small className="text-muted">{post.author.username}</small>
+                                    <small className="text-muted">Posted by {post.author.username}</small>
                                 </p>
                             </div>
-
-                            {/* Comments Section */}
                             <div className="card-footer">
                                 {comments[post._id] &&
                                     comments[post._id].map((comment) => (
                                         <div key={comment._id} className="mb-2">
-                                            <strong>{comment.autor.username}:</strong><p className="mb-0">{comment.text}</p>
+                                            <strong>{comment.autor.username}:</strong>
+                                            <p className="mb-0">{comment.text}</p>
                                         </div>
                                     ))}
                             </div>
-
-                            {/* Render comment input only if token is valid */}
                             {renderCommentInput(post)}
                         </div>
                     </div>
