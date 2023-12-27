0dd0e1778aa082d68aa730f664cdd6dbbf704e2d
Petrov Oleh <81036385+petrovoleh@users.noreply.github.com>
2023-12-27 20:02:48 +0000

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
+const root = document.getElementById('root');
+const reactRoot = createRoot(root);
+
+reactRoot.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>
 );
